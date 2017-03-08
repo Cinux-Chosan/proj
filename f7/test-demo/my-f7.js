f15bcc28fc4ffb0899790efe5a369239937b79c9
Chosan <Chosan>
2017-03-08 17:59:39 +0800

    x

--- f7/test-demo/my-f7.js
+++ f7/test-demo/my-f7.js
@@ -1,27 +1,28 @@
 var myApp = new Framework7();
 var $$ = Framework7.$;
-var mainView = myApp.addView('.view-main', {
+myApp.addView('.view-mainx', {
+  name: "mainx",
   dynamicNavbar: true,
   allowPageChange: true,
   domCache: true
 });
 
-$$(document).on('navbarInit', function(e) {
-  var navbar = e.detail.navbar;
-  var page = e.detail.page;
-});
-
-$$(document).on('navbarReinit', function(e) {
-  var navbar = e.detail.navbar;
-  var page = e.detail.page;
-})
-
-var mySearchbar = myApp.searchbar('.searchbar', {
-    overlay: '.searchbar-overlay'
-});
-
-mySearchbar.onSearch = (function(){debugger});
-mySearchbar.onClear = (function(){debugger});
+// $$(document).on('navbarInit', function(e) {
+//   var navbar = e.detail.navbar;
+//   var page = e.detail.page;
+// });
+//
+// $$(document).on('navbarReinit', function(e) {
+//   var navbar = e.detail.navbar;
+//   var page = e.detail.page;
+// })
+//
+// var mySearchbar = myApp.searchbar('.searchbar', {
+//     overlay: '.searchbar-overlay'
+// });
+//
+// mySearchbar.onSearch = (function(){debugger});
+// mySearchbar.onClear = (function(){debugger});
 
 // setTimeout(function() {mainView.router.loadPage('#second');}, 2000);
 // setTimeout(function() {mainView.router.back();}, 4000);
