4cc16dabee0c0cb641a9254b43974fb7ef19768e
Chosan <Chosan>
2017-03-27 19:02:57 +0800

    x

---
+++ polymer-web-component/z-index-manager.js
@@ -0,0 +1,11 @@
+//
+
+var jenga = (function(global) {
+  'use strict';
+  return {
+    isStackingCtx: function(el) {},
+    getStackingCtx: function(el) {},
+    bringToFront: function(el, createStackingCtx, root) {},
+    sendToBack: function(el, createStackingCtx, root) {}
+  };
+})(this);
